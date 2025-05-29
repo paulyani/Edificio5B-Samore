4b7689db161c60d9dc4515a9ac3da567924e352c
paulyani <paul.perdiguero@gmail.com>
2025-05-29 12:48:01 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,12 @@
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('portero-cache').then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './icon-192.png',
-        './icon-512.png',
-        './manifest.json'
-      ]);
-    })
-  );
+self.addEventListener('install', event => {
+    console.log('Service Worker instalado');
+    self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('activate', event => {
+    console.log('Service Worker activado');
+});
+
+self.addEventListener('fetch', event => {
+    // Caché opcional
 });
