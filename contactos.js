3fd2c7f6904cf63fd645213b4e70bc60c2765b75
paulyani <paul.perdiguero@gmail.com>
2025-10-03 14:53:45 -0300

    Add files via upload

---
+++ contactos.js
@@ -0,0 +1,194 @@
+const buildingContactsData = {
+    "PORTERIA": [
+        {
+            "nombre": "Portería General",
+            "numero": "5491100000001"
+        }
+    ],
+    "PBE": [
+        {
+            "nombre": "Propietario 0E 1",
+            "numero": "5491100000010"
+        }
+    ],
+    "PBF": [
+        {
+            "nombre": "Propietario 0F 1",
+            "numero": "5491100000006"
+        }
+    ],
+    "1D": [
+        {
+            "nombre": "Contacto 1D",
+            "numero": "5491100000008"
+        }
+    ],
+    "1E": [
+        {
+            "nombre": "Persona 1E 1",
+            "numero": "5491100000013"
+        }
+    ],
+    "1F": [
+        {
+            "nombre": "Contacto 1F",
+            "numero": "5491100000014"
+        }
+    ],
+    "2D": [
+        {
+            "nombre": "Contacto 2D",
+            "numero": "5491100000016"
+        }
+    ],
+    "2E": [
+        {
+            "nombre": "Contacto 2E",
+            "numero": "5491100000017"
+        }
+    ],
+    "2F": [
+        {
+            "nombre": "Contacto 2F",
+            "numero": "5491100000018"
+        }
+    ],
+    "3D": [
+        {
+            "nombre": "Contacto 3D",
+            "numero": "5491100000021"
+        }
+    ],
+    "3E": [
+        {
+            "nombre": "Lau",
+            "numero": "5491161722511"
+        }
+    ],
+    "3F": [
+        {
+            "nombre": "Majo",
+            "numero": "5491132066779"
+        }
+    ],
+    "4D": [
+        {
+            "nombre": "Contacto 4D",
+            "numero": "5491100000024"
+        }
+    ],
+    "4E": [
+        {
+            "nombre": "Contacto 4E",
+            "numero": "5491100000025"
+        }
+    ],
+    "4F": [
+        {
+            "nombre": "Contacto 4F 1",
+            "numero": "5491100000026"
+        }
+    ],
+    "5D": [
+        {
+            "nombre": "Contacto 5D 1",
+            "numero": "5491100000028"
+        }
+    ],
+    "5E": [
+        {
+            "nombre": "Contacto 5E 1",
+            "numero": "5491138262836"
+        },
+        {
+            "nombre": "Contacto 5E 2",
+            "numero": "5491156442637"
+        }
+    ],
+    "5F": [
+        {
+            "nombre": "Contacto 5F 1",
+            "numero": "5491170087927"
+        }
+    ],
+    "6D": [
+        {
+            "nombre": "Contacto 6D 1",
+            "numero": "5491100000035"
+        }
+    ],
+    "6E": [
+        {
+            "nombre": "Contacto 6E 1",
+            "numero": "5491100000036"
+        }
+    ],
+    "6F": [
+        {
+            "nombre": "Contacto 6F 1",
+            "numero": "5491100000037"
+        }
+    ],
+    "7D": [
+        {
+            "nombre": "Luciana",
+            "numero": "5491132281904"
+        }
+    ],
+    "7E": [
+        {
+            "nombre": "Contacto 7E 1",
+            "numero": "5491100000039"
+        }
+    ],
+    "7F": [
+        {
+            "nombre": "Contacto 7F 1",
+            "numero": "5491100000040"
+        }
+    ],
+    "8D": [
+        {
+            "nombre": "Contacto 8D 1",
+            "numero": "5491100000041"
+        }
+    ],
+    "8E": [
+        {
+            "nombre": "Contacto 8E 1",
+            "numero": "5491100000042"
+        }
+    ],
+    "8F": [
+        {
+            "nombre": "Contacto 8F 1",
+            "numero": "5491100000043"
+        }
+    ],
+    "9D": [
+        {
+            "nombre": "Contacto 9D 1",
+            "numero": "5491100000044"
+        }
+    ],
+    "9E": [
+        {
+            "nombre": "Contacto 9E 1",
+            "numero": "5491131617118"
+        },
+        {
+            "nombre": "Contacto 9E 2",
+            "numero": "5491166535883"
+        }
+    ],
+    "9F": [
+        {
+            "nombre": "Persona 1",
+            "numero": "5491123312815"
+        },
+        {
+            "nombre": "Persona 2",
+            "numero": "5491150581121"
+        }
+    ]
+};
